ffc45653c009f4b32b94aba933b88693ae78a4ed
DavideGaliano <jconstantin@tiscali.it>
2024-08-29 10:52:30 +0200

    Esercizio + bonus completato

--- src/main/java/org/lessons/java/controller/PizzaController.java
+++ src/main/java/org/lessons/java/controller/PizzaController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/pizze")
@@ -19,14 +21,27 @@ public class PizzaController {
 	
 	
 	@GetMapping()
-	public String index(Model model) {
-		
-		
-		List<Pizza> pizze = repo.findAll();
-		
-		model.addAttribute("pizze", pizze);
-		
-		return "/pizze/index";
+    public String index(@RequestParam(name = "search", required = false) String search, Model model) {
+        List<Pizza> pizze;
+        if (search != null && !search.isEmpty()) {
+            pizze = repo.findByNameContainingIgnoreCase(search);
+        } else {
+            pizze = repo.findAll();  // Recupera tutte le pizze
+        }
+        
+        model.addAttribute("pizze", pizze);
+        model.addAttribute("search", search);  // Mantieni il valore di ricerca nel modello
+        
+        return "/pizze/index";
+    }
+
+	
+	@GetMapping("/{id}")
+	public String show(@PathVariable("id") Integer id, Model model) {
+		model.addAttribute("pizza", repo.findById(id).get());
+		return "/pizze/show";
 	}
+	
+	
 
 }
