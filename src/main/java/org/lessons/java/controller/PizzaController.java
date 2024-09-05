3a6b423d4c1ae4bc8ff218deb39067b1dc27757c
DavideGaliano <jconstantin@tiscali.it>
2024-09-05 13:46:00 +0200

    Esercizio create e validazione completato

--- src/main/java/org/lessons/java/controller/PizzaController.java
+++ src/main/java/org/lessons/java/controller/PizzaController.java
@@ -7,10 +7,15 @@ import org.lessons.java.repo.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.validation.Valid;
 
 @Controller
 @RequestMapping("/pizze")
@@ -42,6 +47,21 @@ public class PizzaController {
 		return "/pizze/show";
 	}
 	
+	@GetMapping("/create")
+	public String create(Model model) {
+		model.addAttribute("pizza", new Pizza());
+		return "/pizze/create";
+	}
 	
+	@PostMapping("/create")
+	public String store(@Valid @ModelAttribute("pizza") Pizza formPizza ,BindingResult bindingResult, Model model)
+	{
+		if(bindingResult.hasErrors()) {
+			return "/pizze/create";
+		}
+		repo.save(formPizza);
+		
+		return "redirect:/pizze";
+	}
 
 }
--- src/main/java/org/lessons/java/model/Pizza.java
+++ src/main/java/org/lessons/java/model/Pizza.java
@@ -1,10 +1,15 @@
 package org.lessons.java.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "pizze")
@@ -14,9 +19,19 @@ public class Pizza {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 	
+	@NotNull
+	@NotEmpty
+	@Size(min=2, max=255)
 	private String name;
+	
+	@NotNull
+	@Size(min=2,max=1000)
+	@Column(length=1000)
 	private String description;
 	private String url;
+	
+	@NotNull
+	@DecimalMin(value = "0.01", message = "Il prezzo deve essere maggiore di zero")
 	private Double prezzo;
 	
 	
